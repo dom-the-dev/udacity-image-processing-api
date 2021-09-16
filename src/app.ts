45c45ad3fd921349095c0c4a8b4fb772e6a64ce6
Dominik <hi@domthedev.com>
2021-09-16 14:53:18 +0200

    add typeScript and jasmine testing

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+const myFunc = ((num: number) => {
+    return num * 5
+})
+
+export default myFunc
---
+++ src/tests/appSpec.ts
@@ -0,0 +1,5 @@
+import myFunc from '../app'
+
+it('should return number to be 20', () => {
+    expect(myFunc(4))
+})
