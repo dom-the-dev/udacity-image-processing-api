d64778be89a791d09972544da91f2b48ecc03474
dom <d.amrugiewicz@sirup.com>
2021-09-20 16:42:59 +0200

    remove deprecated done callback in async/await spec

--- src/tests/routes/apiSpec.ts
+++ src/tests/routes/apiSpec.ts
@@ -7,7 +7,7 @@ const request = supertest(app);
 
 const IMAGE_DIR = "./public/assets/thumbnail";
 
-beforeAll(async () => {
+beforeAll(() => {
   readdir(IMAGE_DIR, (err, files) => {
     if (err) throw err;
 
@@ -20,60 +20,51 @@ beforeAll(async () => {
 });
 
 describe("Test route", () => {
-  it("should response with status 200", async (done: DoneFn) => {
+  it("should response with status 200", async () => {
     const response = await request.get("/api");
-
     expect(response.statusCode).toBe(200);
-    done();
   });
 
-  it("should response Api is running", () => async (done: DoneFn) => {
+  it("should response Api is running", async () => {
     const response = await request.get("/api");
-    expect(response.body).toBe("Api is running");
-    done();
+    expect(response.text).toBe("Api is running");
   });
 });
 
 describe("GET /routes/images", () => {
-  it("should response image not missing filename", async (done: DoneFn) => {
+  it("should response image not missing filename", async () => {
     const response = await request.get("/api/image");
     expect(response.statusCode).toBe(400);
     expect(response.text).toBe("Filename missing");
-    done();
   });
 
-  it("it should response that one parameter is missing", async (done: DoneFn) => {
+  it("it should response that one parameter is missing", async () => {
     const response = await request.get("/api/image?filename=palmtunnel&height=0&width=100");
     expect(response.statusCode).toBe(400);
     expect(response.text).toBe("Values must be greater than zero");
-    done();
   });
 
-  it("it should response that one parameter is missing", async (done: DoneFn) => {
+  it("it should response that one parameter is missing", async () => {
     const response = await request.get("/api/image?filename=palmtunnel&height=200");
     expect(response.statusCode).toBe(400);
     expect(response.text).toBe("Width missing");
-    done();
   });
 
-  it("it should response that one parameter is missing", async (done: DoneFn) => {
+  it("it should response that one parameter is missing", async () => {
     const response = await request.get("/api/image?filename=palmtunnel&width=200");
     expect(response.statusCode).toBe(400);
     expect(response.text).toBe("Height missing");
-    done();
   });
 
-  it("it should response original image", async (done: DoneFn) => {
+  it("it should response original image", async () => {
     const response = await request.get("/api/image?filename=palmtunnel");
     expect(response.statusCode).toBe(200);
-    done();
   });
 
-  it("it should response an thumbnail", async (done: DoneFn) => {
+  it("it should response an thumbnail", async () => {
     const response = await request.get(
       "/api/image?filename=palmtunnel&width=200&height=200"
     );
     expect(response.statusCode).toBe(200);
-    done();
   });
 });
