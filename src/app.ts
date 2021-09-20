64ed7631c9634a54728a9efd02ec8057b8a7ca47
dom <d.amrugiewicz@sirup.com>
2021-09-20 10:13:48 +0200

    fix public files

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,11 @@ import path from "path";
 const app = express();
 const port = 3000;
 
+// Serve static files
+app.use(express.static("public"));
+
 // Api routes
 app.use("/api", routes);
-
-// Serve index.html on root
-app.use("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/index.html"));
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
